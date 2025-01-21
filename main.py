4ff7934cc3abcc4e225d58c6fd3b1eeea9a63de4
Ignacio Ballesteros <igbprogramming@gmail.com>
2025-01-20 20:18:43 -0500

    added a dictionary sorter and a word/char report

--- main.py
+++ main.py
@@ -16,23 +16,41 @@ def char_counter(file_contents):
     return char_dict
 
 
-def list_dict(char_dict):
+def sort_on(dict):
+    return dict["num"]
+
+
+def sort_dict(char_dict):
     list_of_dicts = []
     for char in char_dict:
         list_of_dicts.append({"char": char, "num": char_dict[char]})
 
-    sort_on(list_of_dicts)
+    list_of_dicts.sort(reverse=True, key=sort_on)
+    return list_of_dicts
 
 
-# def report(word_counter, char_counter):
+def report(book, words, dict):
+    print(f"--- Begin report of {book} ---")
+    print(f"{words} words found in the document\n")
+    for element in dict:
+        if (
+            element["char"] != "\n"
+            and element["char"] != " "
+            and element["char"] != "#"
+            and element["char"] != "."
+        ):
+            print(f"The '{element["char"]}' character was found {element["num"]} times")
+    print("--- End report ---")
 
 
 def main():
-    with open("books/frankenstein.txt") as f:
+    book = "books/frankenstein.txt"
+    with open(book) as f:
         file_contents = f.read()
     word_count = word_counter(file_contents)
     char_count = char_counter(file_contents)
-    list_dict(char_counter(file_contents))
+    char_dict = sort_dict(char_count)
+    report(book, word_count, char_dict)
 
 
 main()
